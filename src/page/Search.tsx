4ccb97bd266997a5d32f3a1bd0f94c1e9c37f7af
lpchen <lpcheniris@gmail.com>
2022-05-18 17:50:38 +0800

    add keyupenent for sarch

--- src/page/Search.tsx
+++ src/page/Search.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { render } from 'react-dom';
 import styles from './Search.module.css';
+import CryptoJS from 'crypto-js';
 
 export default function Search() {
+    const [word, setWord] = useState()
+    function handleEnter(e: any) {
+        if (e.key == "Enter") {
+            setWord(e.target.value)
+        }
+    }
+
     return (
         <div className={styles.searchWrapper}>
-            <input  type="text" />
+            <input type="text" onKeyUp={(e) => handleEnter(e)} />
             <button name='search'>Search</button>
-            <button name='collect'>Favorite </button>
+            <button name='collect' >Favorite </button>
         </div>)
 }
